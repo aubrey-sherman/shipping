d8a526f3daf2bfe04339230415dc21771eba60ae
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-21 20:56:21 -0400

    Updated test to mock the API request. Test passes.

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,7 +1,15 @@
 import { test, expect, beforeAll, afterAll, afterEach } from "vitest";
-import { shipViaShipIt, SHIPIT_SHIP_URL } from "./shipItApi.js";
+import { mock, mockServer } from "./apiMock.js";
+import { shipViaShipIt, SHIPIT_SHIP_URL, SHIPIT_API_KEY } from "./shipItApi.js";
+
+beforeAll(function () { mockServer.listen(); });
+afterEach(function () { mockServer.resetHandlers(); });
+afterAll(function () { mockServer.close(); });
 
 test("shipProduct", async function () {
+
+  mock("post", SHIPIT_SHIP_URL, "", { trackingId: 789 });
+
   const trackingId = await shipViaShipIt({
     orderId: 123,
     productId: 1000,
