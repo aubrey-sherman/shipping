84dd368d513c3ed7c9d4b5dd0911562a2cd4162d
Andrea Jao <ndrjao@gmail.com>
2024-05-21 17:19:45 -0700

    updated costs.test.js

--- costs.test.js
+++ costs.test.js
@@ -1,14 +1,29 @@
 import { test, beforeAll, afterAll, expect, vi } from "vitest";
 import { getCost } from "./costs.js";
+import * as discounts from "./discounts.js";
+
+beforeAll(function () {
+  vi.spyOn(discounts, "getRandomDiscount"); // either getCost or costs...
+});
+
+afterAll(function () {
+  vi.resetAllMocks();
+});
 
 test("cheap product", function () {
   // cheap products have IDs between 1000-1999, and cost $100 * discount
+
+  discounts.getRandomDiscount.mockReturnValue(0.1);
+
   const cost = getCost(1200);
-  expect(cost).toBe("90.00");
+  expect(cost).toEqual("90.00");
 });
 
 test("expensive product", function () {
   // expensive products have IDs greater than 2000, and cost $200 * discount
+
+  discounts.getRandomDiscount.mockReturnValue(0.1);
+
   const cost = getCost(2500);
-  expect(cost).toBe("180.00");
+  expect(cost).toEqual("180.00");
 });
